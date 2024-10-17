5e7d8561d87b7829da7fd517cd243163da70bafe
Noravgz <Vergoznora@gmail.com>
2024-10-17 18:09:58 +0200

    game changement

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,30 @@
 class Game
-    def jeu
+attr_accessor :player1_name, :player2_name
+    
 
-    while @tour < 9
-        if @tour.even?  # Si le tour est pair, c'est le joueur 1
-            joueur = "Joueur 1"
-          else  # Si le tour est impair, c'est le joueur 2
-            joueur = "Joueur 2"
-          end
+    def initialize
+        puts "Joueur 1 quel est ton nom ?"
+        @player1_name = gets.chomp
 
-          
+        puts "Joueur 2 quel est ton nom ?"
+        @player2_name = gets.chomp
+
+
+        @cases = {"A1" => " ", "A2" => " ", "A3" => " ",
+            "B1" => " ", "B2" => " ", "B3" => " ",
+            "C1" => " ", "C2" => " ", "C3" => " "}
+end
+
+
+    def afficher_board
+    puts "#{@cases['A1']} | #{@cases['A2']} | #{@cases['A3']}"
+    puts "---------"
+    puts "#{@cases['B1']} | #{@cases['B2']} | #{@cases['B3']}"
+    puts "---------"
+    puts "#{@cases['C1']} | #{@cases['C2']} | #{@cases['C3']}"
+  end
+end
+
+jeu = Game.new
+puts "Bienvenue à vous 2 !!"
+jeu.afficher_board
