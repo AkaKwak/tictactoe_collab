7b5cb1d2a7041ab8a72264e967d2bb7d5b12b91a
AkaKwak <christophe.alaterre@gmail.com>
2024-10-17 13:41:57 +0200

    premier jet

---
+++ lib/show.rb
@@ -0,0 +1,19 @@
+class Show
+
+  def view_boar( , , , , , , , , )  
+    #A1 etc = une instance de BoardCase ?
+    puts "__________________"
+    print ""
+    print ""
+
+    puts ""
+    print ""
+    print""
+
+    puts ""
+    print ""
+    print ""
+
+  end
+  
+end
