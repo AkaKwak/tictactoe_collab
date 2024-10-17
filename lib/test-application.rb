39792329182c041753119ac6afd6ec431e8be0e9
CarolineOlivier <linepro.olivier@gmail.com>
2024-10-17 21:32:21 +0200

    ajout du readme

--- lib/test-application.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require_relative 'application'
-
-class TestApplication < Minitest::Test
-  def test_perform
-    app = Application.new
-    assert app.perform  # On vérifie simplement que l'application s'exécute sans erreur.
-  end
-end
